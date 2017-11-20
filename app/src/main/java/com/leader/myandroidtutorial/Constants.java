0656767f5dc3e11a29cb1f70a295736fd6982d37
Leader <jonathanhsieh.tw@gmail.com>
2017-11-20 23:22:40 +0800

    First commit progress at Android Tutorial 3-1

---
+++ app/src/main/java/com/leader/myandroidtutorial/Constants.java
@@ -0,0 +1,10 @@
+package com.leader.myandroidtutorial;
+
+
+public class Constants {
+    public static final int REQUEST_ADD=0;
+    public static final int REQUEST_EDIT=1;
+    public static final String CLASS_ITEM ="com.leader.myandroidtutorial.Item";
+    public static final String ACTION_ADD_ITEM="com.leader.myandroidtutorial.ADD_ITEM";
+    public static final String ACTION_EDIT_ITEM="com.leader.myandroidtutorial.EDIT_ITEM";
+}
